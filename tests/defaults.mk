484d5b58c4e4d580d5d57f1b57b131cb97657329
Giampiero Gabbiani <giampiero@gabbiani.org>
2023-12-07 15:02:05 +0100

    Updated for Ubuntu integration

--- tests/defaults.mk
+++ tests/defaults.mk
@@ -1,9 +1,9 @@
 # test execution
 %.echo : %.scad
-	@source $(realpath $*.conf) && $(BIN)/make-test.py $$CAMERA $*
+	@. $(realpath $*.conf) && $(BIN)/make-test.py $$CAMERA $*
 
 # source creation
 %.scad : %.conf $(TEMPLATES) $(DEF_CONFS)
-	@echo -n -e "\e[33m$@\e[0m: "
-	@(set -a && source $(DEF_CONFS) && source $(realpath $*.conf) && envsubst <$(TEMPLATE_DIR)/$${TEST_TEMPLATE}) >$@
-	@echo -e "\e[32mcreated\e[0m"
+	@printf "\e[33m$@\e[0m: "
+	@(set -a && . $(DEF_CONFS) && . $(realpath $*.conf) && envsubst <$(TEMPLATE_DIR)/$${TEST_TEMPLATE}) >$@
+	@printf "\e[32mcreated\e[0m\n"
