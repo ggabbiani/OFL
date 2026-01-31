98046aaafe360c7b2d2c8addf54e237d2035bfed
Giampiero Gabbiani <giampiero@gabbiani.org>
2026-01-31 16:43:13 +0100

    Update Dockerfile and make-import.sh to clarify virtual environment activation

--- docker/ubuntu/make-import.sh
+++ docker/ubuntu/make-import.sh
@@ -8,4 +8,9 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
+
+# Attiva il virtual environment in modo che carichi TUTTE le variabili previste da Python
+# Usiamo il punto (.) o source, è indifferente se lo shebang è bash
+source /venv/bin/activate
+
 xvfb-run -a make SHELL="$(which bash)" -s -C /import "$@"
