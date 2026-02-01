25ab409c4636ae35d988807ed8fa3766b12b0161
Giampiero Gabbiani <giampiero@gabbiani.org>
2026-02-01 14:42:15 +0100

    Refactor make-import.sh to improve target directory handling and error messaging

--- docker/ubuntu/make-import.sh
+++ docker/ubuntu/make-import.sh
@@ -13,4 +13,12 @@
 source /venv/bin/activate
 
 echo "GITHUB_WORKSPACE=${GITHUB_WORKSPACE}"
-xvfb-run -a make SHELL="$(which bash)" -s -C "${GITHUB_WORKSPACE}" "$@"
+
+# We use GITHUB_WORKSPACE just as a 'hint', if it exists we use it, if not
+# let's bet on '/import'. If /import doesn't exist either, then rise an error.
+TARGET_DIR="${GITHUB_WORKSPACE:-/import}"
+if [ ! -d "$TARGET_DIR" ]; then
+    echo "Either GITHUB_WORKSPACE or target directory \"${TARGET_DIR}\" doesn't exist."
+    exit 5
+fi
+xvfb-run -d make SHELL="$(which bash)" -s -C "${TARGET_DIR}" "$@"
