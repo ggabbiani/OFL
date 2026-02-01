f3cae7a905def359979209d2016a079fa8108d14
Giampiero Gabbiani <giampiero@gabbiani.org>
2026-02-01 06:55:07 +0100

    Update Dockerfiles and entry scripts to use GITHUB_WORKSPACE for git safe.directory configuration

--- docker/ubuntu/make-import.sh
+++ docker/ubuntu/make-import.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
-# Docker entry-point for Ubuntu environments
+# Docker entry-point for Ubuntu environments. The git repo is meant to be set
+# in GITHUB_WORKSPACE environment variable.
 #
 # This file is part of the 'OpenSCAD Foundation Library' (OFL) project.
 #
@@ -9,8 +10,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 
-# Attiva il virtual environment in modo che carichi TUTTE le variabili previste da Python
-# Usiamo il punto (.) o source, è indifferente se lo shebang è bash
 source /venv/bin/activate
 
-xvfb-run -a make SHELL="$(which bash)" -s -C /import "$@"
+echo "GITHUB_WORKSPACE=${GITHUB_WORKSPACE}"
+xvfb-run -a make SHELL="$(which bash)" -s -C "${GITHUB_WORKSPACE}" "$@"
